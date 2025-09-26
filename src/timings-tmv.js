10d8e5b13fa38db247e3202a65efaf4d72a05a06
Stakovicz <albin@les-tilleuls.coop>
2025-09-26 19:17:35 +0200

    init project

---
+++ src/timings-tmv.js
@@ -0,0 +1,99 @@
+
+const timings = [
+    {
+        name: "Briefing Trail Long 360 Expert Outdoor (63km)",
+        from: "2025-10-05T07:50:00.000+02:00",
+        to: "2025-10-05T08:00:00.000+02:00",
+    },
+    {
+        name: "Départ Trail Long",
+        from: "2025-10-05T08:00:00.000+02:00",
+    },
+
+
+    {
+        name: "Briefing Trail Court Lohmann (31km)",
+        from: "2025-10-05T09:20:00.000+02:00",
+        to: "2025-10-05T09:30:00.000+02:00",
+    },
+    {
+        name: "Départ Trail Court",
+        from: "2025-10-05T09:30:00.000+02:00",
+    },
+
+
+    {
+        name: "Briefing Course U Nature (17km)",
+        from: "2025-10-05T09:50:00.000+02:00",
+        to: "2025-10-05T10:00:00.000+02:00",
+    },
+    {
+        name: "Départ Course Nature",
+        from: "2025-10-05T10:00:00.000+02:00",
+    },
+
+
+    {
+        name: "Briefing Marche Nordique (14km)",
+        from: "2025-10-05T10:05:00.000+02:00",
+        to: "2025-10-05T10:15:00.000+02:00",
+    },
+    {
+        name: "Départ Marche Nordique",
+        from: "2025-10-05T15:00:00.000+02:00",
+    },
+
+
+    {
+        name: "Briefing Course enfants [2016-2018] (1km)",
+        from: "2025-10-05T10:25:00.000+02:00",
+        to: "2025-10-05T10:30:00.000+02:00",
+    },
+    {
+        name: "Départ Course enfants 1km",
+        from: "2025-10-05T10:30:00.000+02:00",
+    },
+
+
+    {
+        name: "Briefing Course enfants [2014-2015] (2km)",
+        from: "2025-10-05T10:40:00.000+02:00",
+        to: "2025-10-05T10:45:00.000+02:00",
+    },
+    {
+        name: "Départ Course enfants 2km",
+        from: "2025-10-05T10:45:00.000+02:00",
+    },
+
+
+
+
+
+    {
+        name: "Podium Courses enfants",
+        from: "2025-10-05T11:15:00.000+02:00",
+        to: "2025-10-05T11:30:00.000+02:00",
+    },
+    {
+        name: "Podium Course Nature",
+        from: "2025-10-05T12:15:00.000+02:00",
+        to: "2025-10-05T12:25:00.000+02:00",
+    },
+    {
+        name: "Podium Marche Nordique",
+        from: "2025-10-05T12:30:00.000+02:00",
+        to: "2025-10-05T12:40:00.000+02:00",
+    },
+    {
+        name: "Podium Trail court",
+        from: "2025-10-05T12:45:00.000+02:00",
+        to: "2025-10-05T12:55:00.000+02:00",
+    },
+    {
+        name: "Podium Trail long",
+        from: "2025-10-05T15:30:00.000+02:00",
+        to: "2025-10-05T15:40:00.000+02:00",
+    },
+];
+
+export { timings as default};
