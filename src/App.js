6e33b94295d0a8475fa65e3aec8da6f22d74391b
Stakovicz <albin@les-tilleuls.coop>
2025-09-30 17:02:50 +0200

    somes fixes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import 'moment/locale/fr';
 
 import './App.css';
 import {useEffect, useState} from "react";
-//import timings from "./timings"
-import timings from "./timings-tmv"
+import tests from "./timingsTest"
+import tmv from "./timings-tmv"
 
 function Ftn({number, unit, hideIfZero = true}) {
     if (hideIfZero && !number) {
@@ -23,14 +23,17 @@ function Upcoming({current, timings}) {
 
     return <div className="upcoming">
         {timings.map((timing, i) => {
-            const duration = timing.to ? moment.duration(moment(timing.to).diff(timing.from)).minutes() : null;
+            const duration = timing.to ? moment.duration(timing.to.diff(timing.from)).minutes() : null;
             const passed = i < nextIndex;
+            if (passed) {
+                return null;
+            }
 
             return (<div key={i} className={`${passed ? "passed" : ""}`}>
                 <div className="datetime">
                     <div className="name">{timing.name}</div>
-                    {moment(timing.from).format("HH:mm")}
-                    {timing.to ? (moment(timing.to).format(" – HH:mm ")) : null}
+                    {timing.from.format("HH:mm")}
+                    {timing.to ? (timing.to.format(" – HH:mm ")) : null}
                     {duration ? <span>[{duration}min]</span> : null}
                 </div>
             </div>)
@@ -40,11 +43,24 @@ function Upcoming({current, timings}) {
 
 function Current({now, timing}) {
     const [rest, setRest] = useState(moment.duration());
+    const [inTalk, setInTalk] = useState(false)
     useEffect(() => {
         if (!timing) {
             return;
         }
-        setRest(moment.duration(moment(timing.from).diff()));
+        let aim;
+        if (timing.from.diff(now) < 0) {
+            aim = timing.to;
+        } else {
+            aim = timing.from;
+        }
+        //aim = aim.add(1, "seconds");
+        setRest(moment.duration(aim.diff(now)));
+
+        if (timing.to) {
+            setInTalk(now.isBetween(timing.from, timing.to));
+        }
+
     }, [now, timing]);
 
     if (!timing) {
@@ -55,40 +71,66 @@ function Current({now, timing}) {
         return null;
     }
 
-    const duration = timing.to ? moment.duration(moment(timing.to).diff(timing.from)).minutes() : null;
+    const duration = timing.to ? moment.duration(timing.to.diff(timing.from)).minutes() : null;
     const color = rest.asMinutes() <= 10 ? rest.asMinutes() <= 5 ? "red" : "orange" : "";
 
     return <div className="App-next">
         <div className="rest" style={{color: color}}>
-            <Ftn number={rest.hours()} unit="h" />
-            <Ftn number={rest.minutes()} unit="min" />
-            <Ftn number={rest.seconds()} unit="s" hideIfZero={false} />
+            {inTalk && "🗣️ "}
+            <Ftn number={rest.days()} unit="j"/>
+            <Ftn number={rest.hours()} unit="h"/>
+            <Ftn number={rest.minutes()} unit="min"/>
+            <Ftn number={rest.seconds()} unit="s" hideIfZero={false}/>
         </div>
         <div className="datetime">
             <div className="name">{timing.name}</div>
-            {moment(timing.from).format("HH:mm")}
-            {timing.to ? (moment(timing.to).format(" – HH:mm ")) : null}
+            {timing.from.format("HH:mm")}
+            {timing.to ? (timing.to.format(" – HH:mm ")) : null}
             {duration ? <span>[{duration}min]</span> : null}
         </div>
     </div>
 }
 
+function setDataTest({setTimings}) {
+    setTimings(tests.map((track) => ({
+        name: track.name,
+        from: moment(track.from),
+        to: track.to ? moment(track.to) : undefined
+    })));
+}
+
+function setData({setTimings}) {
+    setTimings(tmv.map((track) => ({
+        name: track.name,
+        from: moment(track.from),
+        to: track.to ? moment(track.to) : undefined
+    })));
+}
 
 function App() {
     const [now, setNow] = useState(moment());
-    const [current, setCurrent] = useState()
+    const [current, setCurrent] = useState();
+    const [timings, setTimings] = useState([]);
+
+    useEffect(() => {
+        setData({setTimings});
+        //setDataTest({setTimings, setTracks});
+    }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setNow(moment());
 
             setCurrent(timings.find((timing) => {
-                return moment(timing.from).diff(now) >= 0;
+                const aim = timing.to ?? timing.from;
+                return moment(aim).diff(now) > 0;
             }));
         }, 1000);
 
-        return () => {clearInterval(interval)}
-    }, [now]);
+        return () => {
+            clearInterval(interval)
+        }
+    }, [now, timings]);
 
     return (
         <div className="App">
@@ -97,14 +139,15 @@ function App() {
                     <div className="date">
                         {now.format("ddd DD MMM YYYY")}
                     </div>
+
                     <div className="time">
                         {now.format("HH:mm:ss")}
                     </div>
                 </div>
             </header>
             <main className="App-main">
-                <Current now={now} timing={current} />
-                <Upcoming current={current} timings={timings} />
+                <Current now={now} timing={current}/>
+                <Upcoming current={current} timings={timings}/>
             </main>
         </div>
     );
